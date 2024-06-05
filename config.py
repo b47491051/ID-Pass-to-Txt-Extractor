7c9a10f151ae0003acc95ffa70cf9c505a6b0838
b47491051 <b47491051@gmail.com>
2024-06-05 21:52:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id =546431313
+api_id =24519365
 api_hash = ""
-bot_token = "aaf4235465a4r3a1f3af13af1a4fa"
-auth_users = "46513214"
+bot_token = "7176989009:AAFjF3HX84ZK5ZEoU3sh_potj3j91YrEJLY"
+auth_users = "5915548628"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "56416564654"
+osowner_users = "5915548628"
 owner_users = [int(num) for num in osowner_users.split(",")]
